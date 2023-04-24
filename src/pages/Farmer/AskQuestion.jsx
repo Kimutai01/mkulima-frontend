1d10d04299d9aca750315f4c559b062bc1029a35
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-04-24 07:21:42 +0300

    Added chat styling

--- src/pages/Farmer/AskQuestion.jsx
+++ src/pages/Farmer/AskQuestion.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
 
 const AskQuestion = () => {
-  return <div className="pt-28">AskQuestion</div>;
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello I am a farmer",
+      sender: "ChatBot",
+    },
+  ]);
+
+  const handleSend = async (message)=>{
+    const newMessage= {
+      message: message,
+      sender: "user",
+      direction: "outgoing",
+    }
+    const newMessages = [...messages, newMessage]
+    // update message state
+    setMessages(newMessages)
+
+    //process message to chatgpt
+  }
+  return (
+    <div className="pt-28">
+      <div className="h-[80%] w-[70%]">
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map((message, index) => (
+                <Message model={message} key={index} />
+              ))}
+            </MessageList>
+            <MessageInput
+              placeholder="Type message here"
+              onSend={handleSend}
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
